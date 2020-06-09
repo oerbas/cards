bae18c0f44eab85386a838469739c67572e1c3ed
oerbas <orhan1041>
2020-06-10 01:40:33 +0300

    implemented the cards app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      img: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      img: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture'
+    },
+    {
+      title: 'Mountain Biking',
+      img: 'assets/biking.jpeg',
+      username: 'biking12',
+      content: 'Biking of the mountain'
+    }
+  ];
 }
